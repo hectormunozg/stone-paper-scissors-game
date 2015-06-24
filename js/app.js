4df5baaff18224f96717e2db88c2fef3af5da1c2
hectormunozg <hectormunozgarcia@gmail.com>
2015-06-24 07:52:47 +0100

    adding initial functions

--- js/app.js
+++ js/app.js
@@ -6,3 +6,25 @@ var scissors = "img/scissors.svg";
 var paper = "img/paper.svg";
 var gameStarted = false;
 
+// functions
+var showBoard = function() {
+	$('#gamePlayerBoard').show();
+	$('#gameComputerBoard').show();
+	$('#gameStatsWrap').show();	
+};
+
+var rouletteEffect = function() {
+    setInterval(function(){
+      $('#computerChoose li:first-child').fadeOut(0)
+         .next('li').fadeIn(70)
+         .end().appendTo('#computerChoose');}, 80);	
+};
+
+var gameRoulette = function() {
+    $(function(){
+        $('#computerChoose li:gt(0)').hide();
+        rouletteEffect();
+	});	
+}
+
+
